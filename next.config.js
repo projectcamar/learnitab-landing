a1ef70f08b3d9daf2c9f9056a0e803909c8389db
Rio Anggara <rioanggaraclub@gmail.com>
2024-11-07 03:11:09 +0700

    please work okay

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['od.lk', 'e7.pngegg.com'], // Added the new domain
   },
+  // If you're using rewrites/redirects:
+  // assetPrefix: '/',
+  // trailingSlash: true,
 }
 
 module.exports = nextConfig 
