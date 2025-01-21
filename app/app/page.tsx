ae7e54da9b9ccd0890ad3c466642e47e34f24023
Rio Anggara <rioanggaraclub@gmail.com>
2025-01-21 16:30:30 +0700

    wew

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1209,7 +1209,13 @@ export default function Home() {
 
   return (
     <CustomErrorBoundary>
-      <div className="h-screen overflow-hidden w-full flex flex-col" style={{ background: 'transparent' }}>
+      <div 
+        className="h-screen overflow-hidden w-full flex flex-col" 
+        style={{ 
+          background: 'transparent !important',
+          backgroundColor: 'transparent !important'
+        }}
+      >
         {/* Header - remove bg-white */}
         <header className="relative sticky top-0 z-50" style={{ background: 'transparent' }}>
           <div className="relative z-20 w-full px-4 py-4" style={{ background: 'transparent' }}>
